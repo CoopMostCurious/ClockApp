6f987f851e419b274f7c27d53edd1d9cb421fea0
Cooper Hanke <hanke31314@gmail.com>
2018-02-18 21:07:04 -0600

    finished with the script

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,23 @@
 (function() {
     'use strict';
-    console.log('Script connected');
+
+    function clock() {
+        const fullDate = new Date();
+        var hours = fullDate.getHours();
+        var minutes = fullDate.getMinutes();
+        var seconds = fullDate.getSeconds();
+        if (seconds < 10) {
+            seconds = '0' + seconds;
+        }
+        if (minutes < 10) {
+            minutes = '0' + minutes;
+        }
+        if (hours.length < 10) {
+            hours = '0' + hours;
+        }
+        document.getElementById('second').innerHTML = seconds;
+        document.getElementById('minute').innerHTML = minutes + ':';
+        document.getElementById('hour').innerHTML = hours + ':';
+    }
+    setInterval(clock, 1000);
 })();
